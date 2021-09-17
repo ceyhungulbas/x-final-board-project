110f2448d8cefc7cf29a12492d5024ba491badf4
Ceyhun Gulbas <44509662+ceyhungulbas@users.noreply.github.com>
2021-09-17 20:03:13 +0300

    Fix: change aboutPage

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ const About = () => {
     <div>
       <h1>About</h1>
       <p id="aboutPTag">
-        We tried to solve Fetch part for 2 weeks. If you dont like this About
-        Page we can change it to much much professional thing!
+        Hey y'all! Honestly we didn't care about design that much. We focused on functionality. This is a kanban board for personal trainers. You can add or 
+    edit a client, then add exercise to the day you want. Enjoy! <br /> Used technologies: React, Firebase, React Bootstrap.
       </p>
     </div>
   );
